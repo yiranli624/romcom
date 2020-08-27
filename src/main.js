f01d40074a3ba83a5dc6b60ea845ccb7c0953b7c
Daniel Allen <danielallen542@gmail.com>
2020-08-26 20:33:05 -0600

    Add saved covers view to iteration2

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ var newCoverImage = document.querySelector('.cover-image');
 var newTitle = document.querySelector('.cover-title');
 var newTaglineOne = document.querySelector('.tagline-1');
 var newTaglineTwo = document.querySelector('.tagline-2');
-var homePageView = document.querySelector('.home-view');
+var homePageLocation = document.querySelector('.home-view');
 var makeNewFormLocation = document.querySelector('.form-view');
+var viewSavedCoverLocation = document.querySelector('.saved-view');
 var homeButton = document.querySelector('.home-button');
 var randomCoverButton = document.querySelector('.random-cover-button');
 var saveCoverButton = document.querySelector('.save-cover-button');
 var makeNewFormButton = document.querySelector('.make-new-button');
-
+var viewSavedCoversButton = document.querySelector('.view-saved-button');
 
 // We've provided a few variables below
 var savedCovers = [
@@ -22,7 +23,7 @@ var currentCover;
 // were looking out for how to use ONLOAD and where
 randomCoverButton.addEventListener('click', showNewRandomCover);
 makeNewFormButton.addEventListener('click', createNewCover);
-
+viewSavedCoversButton.addEventListener('click', viewSavedCovers);
 // Create your event handlers and other functions here 👇
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
@@ -42,13 +43,19 @@ function showNewRandomCover() {
 }
 function createNewCover() {
   makeNewFormLocation.classList.remove('hidden');
-  homePageView.classList.add('hidden');
+  homePageLocation.classList.add('hidden');
   randomCoverButton.classList.add('hidden');
   saveCoverButton.classList.add('hidden');
   homeButton.classList.remove('hidden');
 
 }
-
+function viewSavedCovers() {
+  viewSavedCoverLocation.classList.remove('hidden');
+  homePageLocation.classList.add('hidden');
+  randomCoverButton.classList.add('hidden');
+  saveCoverButton.classList.add('hidden');
+  homeButton.classList.remove('hidden');
+}
 
 
 randomHomePage();
