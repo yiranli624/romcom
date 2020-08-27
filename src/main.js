8c78346ae4070176a177a16e6f5e8bfcf6c1b9e9
Elle Li <yiran.li@hotmail.com>
2020-08-26 20:03:20 -0600

    Finish Form View in Iteration 2

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ var newCoverImage = document.querySelector('.cover-image');
 var newTitle = document.querySelector('.cover-title');
 var newTaglineOne = document.querySelector('.tagline-1');
 var newTaglineTwo = document.querySelector('.tagline-2');
+var homePageView = document.querySelector('.home-view');
+var makeNewFormLocation = document.querySelector('.form-view');
+var homeButton = document.querySelector('.home-button');
 var randomCoverButton = document.querySelector('.random-cover-button');
+var saveCoverButton = document.querySelector('.save-cover-button');
+var makeNewFormButton = document.querySelector('.make-new-button');
+
 
 // We've provided a few variables below
 var savedCovers = [
@@ -14,9 +20,13 @@ var currentCover;
 // Add your event listeners here 👇
 // we need to have the function running once the page is loaded
 // were looking out for how to use ONLOAD and where
-randomCoverButton.addEventListener("click", showNewRandomCover);
+randomCoverButton.addEventListener('click', showNewRandomCover);
+makeNewFormButton.addEventListener('click', createNewCover);
 
 // Create your event handlers and other functions here 👇
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
 function randomHomePage(){
   newCoverImage.src = covers[getRandomIndex(covers)];
   newTitle.innerText= titles[getRandomIndex(titles)];
@@ -30,8 +40,13 @@ function showNewRandomCover() {
   newTaglineOne.innerText = currentCover.tagline1;
   newTaglineTwo.innerText = currentCover.tagline2;
 }
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
+function createNewCover() {
+  makeNewFormLocation.classList.remove('hidden');
+  homePageView.classList.add('hidden');
+  randomCoverButton.classList.add('hidden');
+  saveCoverButton.classList.add('hidden');
+  homeButton.classList.remove('hidden');
+
 }
 
 
